002711f717be61c3a2f18bca574930c7ca914e71
sayed <sayedelhoseny@gmail.com>
2024-06-13 15:34:17 +0300

    finished presentation layer

--- lib/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
@@ -1,22 +1,26 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 import 'package:bookly/Features/home/presentation/manger/featured_books_cubit/featured_books_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit(this.fetchFeaturedBooksUseCase)
-      : super(FeaturedBooksInitial());
-  final FetchFeaturedBooksUseCase fetchFeaturedBooksUseCase;
+  FeaturedBooksCubit(this.featuredBooksUseCase) : super(FeaturedBooksInitial());
 
-  Future<void> fetchFeaturedBooks() async {
-    emit(FeaturedBooksLoading());
-    var result = await fetchFeaturedBooksUseCase.call();
-    result.fold(
-      (failure) {
-        emit(FeaturedBooksFailure(failure.toString()));
-      },
-      (books) {
-        emit(FeaturedBooksSuccess(books));
-      },
-    );
+  final FetchFeaturedBooksUseCase featuredBooksUseCase;
+  Future<void> fetchFeaturedBooks({int pageNumber = 0}) async {
+    if (pageNumber == 0) {
+      emit(FeaturedBooksLoading());
+    } else {
+      emit(FeaturedBooksPaginationLoading());
+    }
+    var result = await featuredBooksUseCase.call(pageNumber);
+    result.fold((failure) {
+      if (pageNumber == 0) {
+        emit(FeaturedBooksFailure(failure.message));
+      } else {
+        emit(FeaturedBooksPaginationFailure(failure.message));
+      }
+    }, (books) {
+      emit(FeaturedBooksSuccess(books));
+    });
   }
 }
